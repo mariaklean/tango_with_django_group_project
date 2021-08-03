15bff4edbb6af333c59ed5c81daafb423e16cc20
Maria <makleanthous@gmail.com>
2021-08-03 09:39:20 +0300

    First commit

---
+++ populate_rango.py
@@ -0,0 +1,73 @@
+import os
+from typing import cast
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+import django
+django.setup()
+from rango.models import Category, Page
+
+def populate():
+
+	python_pages = [
+		{"title": "Official Python Tutorial",
+		"url":"http://docs.python.org/3/tutorial/",
+		"views":55},
+		{"title":"How to Think like a Computer Scientist",
+		"url":"http://www.greenteapress.com/thinkpython/",
+		"views":12},
+		{"title":"Learn Python in 10 Minutes",
+		"url":"http://www.korokithakis.net/tutorials/python/",
+		"views":63} ]
+
+	django_pages = [
+		{"title":"Official Django Tutorial",
+		"url":"https://docs.djangoproject.com/en/2.1/intro/tutorial01/",
+		"views":20},
+		{"title":"Django Rocks",
+		"url":"http://www.djangorocks.com/",
+		"views":25},
+		{"title":"How to Tango with Django",
+		"url":"http://www.tangowithdjango.com/",
+		"views":27} ]
+
+	other_pages = [
+		{"title":"Bottle",
+		"url":"http://bottlepy.org/docs/dev/",
+		"views":80},
+		{"title":"Flask",
+		"url":"http://flask.pocoo.org",
+		"views":75} ]
+
+	cats = {"Python": {"pages": python_pages, "views": 128,"likes":64},
+		"Django": {"pages": django_pages,"views": 64,"likes":32},
+		"Other Frameworks": {"pages": other_pages, "views": 32,"likes":16} }
+
+
+	for cat, cat_data in cats.items():
+		c = add_cat(cat, cat_data["views"], cat_data["likes"])
+		for p in cat_data["pages"]:
+			add_page(c, p["title"], p["url"], p["views"])
+
+	# Print out the categories we have added.
+	for c in Category.objects.all():
+		for p in Page.objects.filter(category=c):
+			print(f'- {c}: {p}')
+
+def add_page(cat, title, url, views):
+	p = Page.objects.get_or_create(category=cat, title=title, views=views)[0]
+	p.url=url
+	p.views=views
+	p.save()
+	return p
+
+def add_cat(name, likes=0, views=0):
+	c = Category.objects.get_or_create(name=name)[0]
+	c.likes = likes
+	c.views = views
+	c.save()
+	return c
+
+# Start execution here!
+if __name__ == '__main__':
+	print("Starting Rango population script...")
+	populate()
