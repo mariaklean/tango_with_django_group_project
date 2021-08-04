5aad8a6dddb569dca26a34520d35f8eeb57f73ae
Maria <makleanthous@gmail.com>
2021-08-04 16:36:11 +0300

    adding authorization during login

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -43,8 +43,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rango'
+    'rango',
+    'registration', 
 ]
+# If True, users can register.
+REGISTRATION_OPEN = True
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN = True
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = 'rango:index'
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL = 'auth_login'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -107,7 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LOGIN_URL = 'rango:login'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -20,9 +20,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rango import views 
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-    
+    path('accounts/', include('registration.backends.simple.urls')),
     path('admin/', admin.site.urls),
+
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
